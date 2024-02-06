20aa2c93ca0de72b484ff7a58ccd62f8954d5908
PS227368 <93662513+PS227368@users.noreply.github.com>
2024-02-06 11:49:20 +0100

    understructure HomeScreen

--- resources/views/understructure.blade.php
+++ resources/views/understructure.blade.php
@@ -4,17 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welkom bij onze webshop</title>
-    <!-- Voeg eventuele stijlen hier toe -->
+    <!-- Bootstrap CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Voeg eventuele andere aangepaste stijlen hier toe -->
+    <style>
+        /* Voeg eventuele aangepaste stijlen hier toe */
+        /* Dit is een voorbeeld van hoe je de body kunt centreren met flexbox */
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+    </style>
 </head>
 <body>
-    <div>
+    <div class="container text-center">
         <h1>Welkom bij onze webshop</h1>
         <p>Binnenkort komt hier een nieuwe website met een webshop!</p>
-        <img src="{{ asset('') }}" alt="Winkel in Nuenen">
-        <h2>Adresgegevens van onze vestiging:</h2>
+        <img src="/afb/KUINlogo.png" alt="Logo" class="img-fluid">
+        <h2>Adresgegevens van onze vestiging:</h2>  
         <ul>
             <li>Vestiging 1 - Adres 1, Tel: 123456789</li>
         </ul>
     </div>
+    
 </body>
 </html>
