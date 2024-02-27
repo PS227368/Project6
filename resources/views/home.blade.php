798ff9255e40202ca163764dfc4143a567cf3f60
PS229589 <PS229589@summacollege.nl>
2024-02-27 13:50:52 +0100

    slider toegevoegd

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,27 +1,18 @@
-<!-- resources/views/home.blade.php -->
+<!-- resources/views/product.blade.php -->
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel Product Example</title>
+    <title>Laravel Product Page</title>
 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css">
+    <link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
     <link rel="stylesheet" href="index.css">
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Voeg eventuele andere aangepaste stijlen hier toe -->
-    <style>
-        /* Voeg eventuele aangepaste stijlen hier toe */
-        /* Dit is een voorbeeld van hoe je de body kunt centreren met flexbox */
-            /* body {
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                height: 100vh;
-                margin: 0;
-            } */
-    </style>
-</head>
+
 <body>
 
 <link href="{{ asset('css/navbar.css') }}" rel="stylesheet">
@@ -43,35 +34,67 @@
         </form>
     </div>
 </nav>
-    <h1 style="margin-top: 5.5rem; text-align: center;">Alle producten</h1>
 
-    <!-- Filter Form -->
-    <form style="text-align: center;" action="{{ url('/index') }}" method="GET">
-        <label for="name">Filter by Name:</label>
-        <input type="text" name="name" id="name" value="{{ request('name') }}">
-        <button type="submit">Filter</button>
-    </form>
+<h1 class="page-heading">Welkom op onze productpagina</h1>
 
-    <!-- Product Table -->
-    <table style="margin: auto;" border="1">
-        <thead>
+<!-- Slider van producten -->
+<div class="product-slider">
+    @foreach($products as $product)
+        <div class="product-item">
+            <img src="{{ $product->image }}" alt="{{ $product->name }}" class="img-fluid">
+            <h3>{{ $product->name }}</h3>
+            <p>{{ $product->description }}</p>
+            <p class="price">{{ $product->price }}</p>
+            <!-- Voeg hier andere productinformatie toe indien nodig -->
+        </div>
+    @endforeach
+</div>
+
+<!-- Filter Form -->
+<form class="filter-form" action="{{ url('/index') }}" method="GET">
+    <label for="name">Filter by Name:</label>
+    <input type="text" name="name" id="name" value="{{ request('name') }}">
+    <button type="submit">Filter</button>
+</form>
+
+<!-- Product Table -->
+<!-- <table class="product-table" border="1">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Price</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($products as $product)
             <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Description</th>
-                <th>Price</th>
+                <td>{{ $product->id }}</td>
+                <td>{{ $product->name }}</td>
+                <td>{{ $product->description }}</td>
+                <td>{{ $product->price }}</td>
             </tr>
-        </thead>
-        <tbody>
-            @foreach($products as $product)
-                <tr>
-                    <td>{{ $product->id }}</td>
-                    <td>{{ $product->name }}</td>
-                    <td>{{ $product->description }}</td>
-                    <td>{{ $product->price }}</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+        @endforeach
+    </tbody>
+</table> -->
+
+<!-- JavaScript voor Slick Slider -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
+<script>
+    $(document).ready(function(){
+        $('.product-slider').slick({
+            slidesToShow: 3, // Pas het aantal getoonde slides aan
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000, // Pas de snelheid van de automatische weergave aan
+            arrows: true,
+            dots: true
+        });
+    });
+</script>
+
 </body>
 </html>
