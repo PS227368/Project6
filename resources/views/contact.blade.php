f48618149c9b196528ca99d5c16f9d025116abf2
PS227368 <93662513+PS227368@users.noreply.github.com>
2024-03-01 11:29:48 +0100

    Update contact.blade.php
    text aanpassing/ werkende knop verzenden

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -28,8 +28,8 @@
 
     <div class="container mx-auto flex flex-col lg:flex-row justify-between items-start lg:items-center mt-8">
         <div class="w-full lg:w-1/2">
-            <h1 class="text-4xl font-bold text-center lg:text-left">Contactpagina</h1>
-            <p class="text-center lg:text-left">Route naar bosweg 34, Best</p>
+            <h1 class="text-4xl font-bold text-center lg:text-center">Contactpagina</h1>
+            <p class="text-center lg:text-center">Route naar bosweg 34, Best</p>
             <div id="map" class="mt-4">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3740.293192039574!2d5.405722577649258!3d51.541546109077466!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c6dd991962cda9%3A0x40322e05f11ef9e2!2sBosweg%2C%20Best!5e0!3m2!1snl!2snl!4v1707474397065!5m2!1snl!2snl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
